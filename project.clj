a4bebed34c2e629005ba805c61c9b47114e692ef
Alex Guerra <alexander.a.guerra@gmail.com>
2024-07-18 14:01:43 -0500

    chore: Update project.clj with correct description and license information

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject rabbit-hole "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "Advetures of Alice in Wonderland In Clojure"
+  :url "https://github.com/alexguerra-dev/rabbit-hole"
+  :license {:name "The MIT License"
+            :url "https://opensource.org/license/mit"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :main ^:skip-aot rabbit-hole.core
   :target-path "target/%s"
