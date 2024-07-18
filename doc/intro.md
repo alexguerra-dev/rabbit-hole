070ee16e833a166cd1e17d0276c332dae35fd21a
Alex Guerra <alexander.a.guerra@gmail.com>
2024-07-18 13:44:48 -0500

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rabbit-hole
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/rabbit_hole/core_test.clj
@@ -0,0 +1,7 @@
+(ns rabbit-hole.core-test
+  (:require [clojure.test :refer :all]
+            [rabbit-hole.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
