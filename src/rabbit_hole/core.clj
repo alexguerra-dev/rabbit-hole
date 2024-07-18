5452fff388d9f04929b158a2f3038e9d7c6b3407
Alex Guerra <alexander.a.guerra@gmail.com>
2024-07-18 14:23:41 -0500

    chore: Update .gitignore to include /doc/scratch.md and **/.calva/repl.calva-repl files

--- src/rabbit_hole/core.clj
+++ src/rabbit_hole/core.clj
@@ -16,5 +16,11 @@
 ;;;; Notes:
 ;;; These notes are most likely going to be moved out into another document. This is just a temperary place for them to live. I am doing this so I can feel ok writing notes and making the code look ugly. If I can return and clean it up later. That is good because I can get the ideas out of my head and into a place where I can see them.
 
-
+(defn rabbit-hole
+  "This is the main function that will be called to start the game."
+  []
+  (println "Welcome to the Rabbit Hole!")
+  (println "You are Alice and you have fallen down the rabbit hole. This is the start of your adventure.")
+  (println "You are sitting on the branch bored and you see a rabbit run by.")
+  (println "Do you follow the rabbit? (yes/no)"))
 
