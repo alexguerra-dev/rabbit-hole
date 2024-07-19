7de8c7190c8c1024d9d1b3f4bafd0ac93bfca7f1
Alex Guerra <alexander.a.guerra@gmail.com>
2024-07-19 09:46:55 -0500

    feat: Add character data and functions to core.clj

---
+++ src/rabbit_hole/notes.clj
@@ -0,0 +1,1 @@
+(ns rabbit-hole.notes)
---
+++ src/rabbit_hole/wonderland/characters.clj
@@ -0,0 +1,37 @@
+(ns rabbit-hole.wonderland.characters)
+
+
+;; There are many characters in the story. She goes on an adventure. It is filled with small interactions with many characters. They each have big personalitys. They are all very different. They are all very strange. They are all very interesting. They are all very fun. They are all very scary. I think this is one of the things that makes it a good story. I guess on the grounds that all the characters are scary. I would have to push back. I think they are all definitly strange. I think some are scary. Some are just interesting.
+(def characters ["Alice" "Rabbit" "Mad Hatter" "Queen of Hearts" "Cheshire Cat" "Caterpillar" "Dormouse" "March Hare" "Tweedledee" "Tweedledum"])
+
+
+;; Questions I can ask about the data of characters
+;; How many characters are there?
+;; What are the names of the characters?
+
+(defn number-of-characters
+  "This function will return the number of characters in the characters list."
+  []
+  (count characters))
+
+(def alice {:name       "Alice"
+            :age        7
+            :size       "small"
+            :location   "England"
+            :adventures 0
+            :cat        "Dinah"
+            :sister     "Lorina"
+            :bio        "Alice is a curious and imaginative"
+            :stats      {:strength     5
+                         :intelligence 7
+                         :wisdom       6
+                         :dexterity    4
+                         :constitution 5
+                         :charisma     6}})
+
+(def rabbit {:name "Rabbit" :size "small" :location "Wonderland" :adventures 0})
+
+(defn random-character
+  "This function will return a random character from the characters list."
+  []
+  (rand-nth characters))
